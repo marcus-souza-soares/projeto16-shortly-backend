53cb65139477ec47814326739a1398621584c05f
Marcus <marcusadrenaline@gmail.com>
2022-08-04 12:00:27 -0300

    rota de login implementada com validacao de usuario

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,17 +1,29 @@
 import jwt from 'jsonwebtoken';
 import connection from '../db/db.js';
-import bcrypt from 'bcrypt' 
+import bcrypt from 'bcrypt';
 
 export async function SignIn(req, res) {
-    const user = req.body;
+    const {email, password} = req.body;
 
     const secret_key = process.env.JWT_SECRET;
     const configToken = { expiresIn: 60 * 60 * 3 }
+    const { rows: verifyUserExists } = await connection.query(
+        "SELECT * FROM users WHERE email = $1", [email]
+    )
+    const compare_password = bcrypt.compareSync(password, verifyUserExists[0].password);
+    if(verifyUserExists.length < 1 || !compare_password){
+        return res.status(401).send("Verifique os campos")
+    }
+    const userId = verifyUserExists[0].id;
 
-    const { email, password } = user;
-
-    const userFromDatabase = await connection.query(`
-    SELECT`)
+    const token = jwt.sign({ userId }, secret_key, configToken);
+    await connection.query(
+        `INSERT INTO sessions (token, "userId") VALUES ('${token}', ${userId})
+        `
+    )
+    res.status(200).send(token);
+    // const userFromDatabase = await connection.query(`
+    // SELECT`)
 }
 
 export async function creatUser(req, res) {
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import { schemaValidate } from "../middlewares/schemaValidate.js";
 import { schemaSignin, schemaSignup } from "../schemas/authSchema.js";
-import { teste, creatUser } from "../controllers/authController.js"
+import { SignIn, creatUser } from "../controllers/authController.js"
 
 const router = Router();
 
 router.post("/signup", schemaValidate(schemaSignup), creatUser);
-router.post("/signin", schemaValidate(schemaSignin), teste);
+router.post("/signin", schemaValidate(schemaSignin), SignIn);
 
 export default router;
