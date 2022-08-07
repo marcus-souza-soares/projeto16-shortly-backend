2383bc384cf762a855e541be7833c8828af4f542
Marcus <marcusadrenaline@gmail.com>
2022-08-07 11:16:45 -0300

    subindo pro heroku

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -4,44 +4,56 @@ import { authRepository } from '../repository/authRepository.js';
 import { urlRepository } from '../repository/urlRepository.js';
 
 export async function SignIn(req, res) {
-    const {email, password} = req.body;
+    const { email, password } = req.body;
 
     const secret_key = process.env.JWT_SECRET;
     const configToken = { expiresIn: 60 * 60 * 3 }
+    try {
 
-    const { rows: verifyUserExists} = await authRepository.getUserByEmail(email)
+        const { rows: verifyUserExists } = await authRepository.getUserByEmail(email)
 
-    console.log(verifyUserExists)
-    const compare_password = bcrypt.compareSync(password, verifyUserExists[0].password);
-    if(verifyUserExists.length < 1 || !compare_password){
-        return res.status(401).send("Verifique os campos");
+        console.log(verifyUserExists)
+        const compare_password = bcrypt.compareSync(password, verifyUserExists[0].password);
+        if (verifyUserExists.length < 1 || !compare_password) {
+            return res.status(401).send("Verifique os campos");
+        }
+        const userId = verifyUserExists[0].id;
+
+        const token = jwt.sign({ userId }, secret_key, configToken);
+        await authRepository.incrementSession(token, userId);
+        res.status(200).send(token);
+    } catch (error) {
+        res.sendStatus(500);
     }
-    const userId = verifyUserExists[0].id;
-
-    const token = jwt.sign({ userId }, secret_key, configToken);
-    await authRepository.incrementSession(token, userId);
-    res.status(200).send(token);
 }
 
 export async function creatUser(req, res) {
     const { name, email, password } = req.body;
-    const { rows: verify_email } = await authRepository.getUserByEmail(email);
+    try {
+        const { rows: verify_email } = await authRepository.getUserByEmail(email);
 
-    if (verify_email.length > 0){
-        return res.status(409).send("usuário existente!")
+        if (verify_email.length > 0) {
+            return res.status(409).send("usuário existente!")
+        }
+        const crypted_password = bcrypt.hashSync(password, 10);
+
+        await authRepository.createUser(name, email, crypted_password)
+        res.status(201).send("Ok");
+    } catch (error) {
+        res.sendStatus(500);
     }
-    const crypted_password = bcrypt.hashSync(password, 10);
-
-    await authRepository.createUser(name, email, crypted_password)
-    res.status(201).send("Ok");
 }
-export async function getUserUrlsData(req, res){
+export async function getUserUrlsData(req, res) {
     const { userId } = res.locals;
-    const { rows: user } = await authRepository.getUserById(id);
-    if(user.length < 1){
-        return res.status(404).send("Usuário não encontrado.")
+    try {
+        const { rows: user } = await authRepository.getUserById(userId);
+        if (user.length < 1) {
+            return res.status(404).send("Usuário não encontrado.")
+        }
+        console.log(userId)
+        const dataOfUser = await urlRepository.getUserDataAndUrls(userId);
+        res.send(dataOfUser)
+    } catch (error) {
+        res.sendStatus(500);
     }
-    console.log(userId)
-    const dataOfUser = await urlRepository.getUserDataAndUrls(userId);
-    res.send(dataOfUser)
 }
--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -9,59 +9,79 @@ export async function postUrl(req, res) {
     const { userId } = res.locals;
 
     const shortUrl = nanoid(9);
-    await urlRepository.insertNewShortUrl(shortUrl, url, userId);
-    res.status(201).send(
-        {
-            shortUrl
-        }
-    )
+    try {
+        await urlRepository.insertNewShortUrl(shortUrl, url, userId);
+        res.status(201).send(
+            {
+                shortUrl
+            }
+        )
+    } catch (error) {
+        res.sendStatus(500)
+    }
 }
 
 export async function getUrlById(req, res) {
     const { id } = req.params;
 
-    const url = await urlRepository.selectUrlById(id);
+    try {
+        const url = await urlRepository.selectUrlById(id);
 
-    if (url.rowCount === 0) {
-        return res.status(404).send("URL não encontrada")
+        if (url.rowCount === 0) {
+            return res.status(404).send("URL não encontrada")
+        }
+        const [dados] = url.rows;
+        res.status(200).send({
+            id: dados.id,
+            shortUrl: dados.shortUrl,
+            url: dados.url
+        })
+    } catch (error) {
+        res.sendStatus(500)
     }
-    const [dados] = url.rows;
-    res.status(200).send({
-        id: dados.id,
-        shortUrl: dados.shortUrl,
-        url: dados.url
-    })
 }
 
 export async function getShortUrlByName(req, res) {
     const { shortUrl } = req.params;
-    const result = await urlRepository.selectShortUrlByName(shortUrl)
-    if (result.rowCount === 0) {
-        return res.status(404).send("URL não encontrada");
+    try {
+        const result = await urlRepository.selectShortUrlByName(shortUrl)
+        if (result.rowCount === 0) {
+            return res.status(404).send("URL não encontrada");
+        }
+        const [link] = result.rows;
+        await urlRepository.incrementViewShortUrl(link.id)
+        res.redirect(link.url);
+    } catch (error) {
+        res.sendStatus(500);
     }
-    const [link] = result.rows;
-    await urlRepository.incrementViewShortUrl(link.id)
-    res.redirect(link.url);
 }
 
 export async function deleteUrl(req, res) {
     const { id } = req.params;
     const { userId } = res.locals;
 
-    const result = await urlRepository.selectUrlById(id)
-    const id_user = result.rows[0].userId;
-    console.log(result.rows)
-    //caso a url não pertenca ao usuário 
-    if (userId !== id_user){
-        return res.sendStatus(401);
+    try {
+        const result = await urlRepository.selectUrlById(id)
+        const id_user = result.rows[0].userId;
+        console.log(result.rows)
+        //caso a url não pertenca ao usuário 
+        if (userId !== id_user) {
+            return res.sendStatus(401);
+        }
+        if (result.rowCount === 0) {
+            return res.status(404).send("URL não encontrada");
+        }
+        await urlRepository.deleteUrl(id);
+        res.status(204).send("Deletou");
+    } catch (error) {
+        res.sendStatus(500)
     }
-    if (result.rowCount === 0) {
-        return res.status(404).send("URL não encontrada");
-    }
-    await urlRepository.deleteUrl(id);
-    res.status(204).send("Deletou");
 }
-export async function getRanking(req, res){
-    const ranking = await urlRepository.selectUrlsLimited();
-    res.status(200).send(ranking.rows)
+export async function getRanking(req, res) {
+    try {
+        const ranking = await urlRepository.selectUrlsLimited();
+        res.status(200).send(ranking.rows)
+    } catch (error) {
+        res.sendStatus(500);
+    }
 }
